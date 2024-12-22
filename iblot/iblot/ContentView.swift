1947e7997cfcc59799b4c76ab71a6387a64349a5
potato dog man <saumil707@gmail.com>
2024-12-22 23:07:52 +0800

    me when i do this thing called basic svg editor

---
+++ iblot/iblot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iblot
+//
+//  Created by Saumil Anand on 22/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iblot/iblot/iblotApp.swift
@@ -0,0 +1,17 @@
+//
+//  iblotApp.swift
+//  iblot
+//
+//  Created by Saumil Anand on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct iblotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
